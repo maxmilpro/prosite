f39f128f3b338e3db7b97007e01913c21ae9c9a6
maxmilpro <proanom89@gmail.com>
2016-04-08 18:23:38 -0700

    Start Project model

--- bids/models.py
+++ bids/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    customer_first_name = models.CharField(max_length=25)
+    customer_last_name = models.CharField(max_length=25)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=35)
+    state = models.CharField(max_length=2)
+    zip_code = models.IntegerField()
