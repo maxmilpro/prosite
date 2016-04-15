705a28545c672631f04f014c0ae38d19327fbc7a
maxmilpro <proanom89@gmail.com>
2016-04-15 11:19:09 -0700

    link home lsitview to proposal detailview

--- bids/urls.py
+++ bids/urls.py
@@ -6,6 +6,6 @@ app_name = 'bids'
 urlpatterns = [
     url(r'^home/$', HomeView.as_view()),
     url(r'^customers/$', CustomerList.as_view()),
-    url(r'^proposal/(?P<pk>[0-9]+)/$', ProposalView.as_view()),
-    url(r'^proposal/(?P<pk>[0-9]+)/sections/$', SectionView.as_view())
+    url(r'^proposal/(?P<pk>[0-9]+)/$', ProposalView.as_view(), name='proposal'),
+    url(r'^proposal/(?P<pk>[0-9]+)/sections/$', SectionView.as_view(), name='section')
 ]
